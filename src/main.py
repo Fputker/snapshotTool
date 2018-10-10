31468ea36f10e0c26c29c7a805b2ce9810ea8c37
Florian Putker <putkerflorian@hotmail.com>
2018-10-10 16:27:53 +0100

    added check for pending snapshots

--- src/main.py
+++ src/main.py
@@ -124,6 +124,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print('snapshot is already pending for {0}'.format(v.id))
+                continue
             print('Creating snapshot of {0}'.format(v.id))
             v.create_snapshot(Description='Created by snapshotTool')
 
@@ -135,6 +138,12 @@ def create_snapshots(project):
     print('Finished creating snapshots')
 
     return
+
+
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+
+    return snapshots and snapshots[0].state == 'pending'
 
 
 @snapshots.command('list')
